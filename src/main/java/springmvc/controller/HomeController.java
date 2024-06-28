55149de9f0bccae52064ae1b26865fbc1f5e0359
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-28 22:39:30 +0530

    spring mvc configurations

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 		System.out.println("home page");
 		return "index";
 	}
-	// ABOUT PAGE
+	// about PAGE
 	@RequestMapping("/about")
 	public String about() {
 		System.out.println("this is about page");
